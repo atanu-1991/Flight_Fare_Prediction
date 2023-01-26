6d732b55ed760c7b90fa6f58c76b2161f99d5111
atanu-1991 <atanukundu1991@gmail.com>
2023-01-27 01:12:17 +0530

    Data Ingestion Completed

--- flight/entity/config_entity.py
+++ flight/entity/config_entity.py
@@ -1,4 +1,56 @@
-class DataIngestionConfig:...
+from flight.logger import logging
+from flight.exception import FlightException
+from datetime import datetime
+import os, sys
+
+FILE_NAME = "flight.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+class TrainingPipelineConfig:
+
+    def __init__(self):
+
+        try:
+            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y_%H%M%S')}")
+        except Exception as e:
+            logging.debug(str(e))
+            raise FlightException(error_message=e, error_detail=sys)
+
+
+
+class DataIngestionConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+
+        try:
+            self.database_name = "flight_fare_prediction"
+            self.collection_name = "flight_fare"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+            self.test_size = 0.2
+
+        except Exception as e:
+
+            logging.debug(str(e))
+            raise FlightException(error_message=e, error_detail=sys)
+
+
+    def to_dict(self)->dict:
+
+        try:
+            return self.__dict__
+
+        except Exception as e:
+            logging.debug(str(e))
+            raise FlightException(error_message=e, error_detail=sys)
+
+
+
+
+
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainingConfig:...
--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-import pymongo
+from flight.logger import logging
+from flight.exception import FlightException
+from flight.utils import get_collection_as_dataframe
+from flight.entity import config_entity
+from flight.components.data_ingestion import DataIngestion
+import sys,os
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+def test_logger_and_exception():
+     try:
+          logging.info("Starting the test_logger_and_exception")
+          result = 3/0
+          print(result)
+          logging.info("Stopping the test_logger_and_exception")
+          
+     except Exception as e:
+          logging.debug(str(e))
+          raise FlightException(e, sys)
 
-# Database Name
-dataBase = client["neurolabDB"]
 
-# Collection  Name
-collection = dataBase['Products']
+if __name__ == "__main__":
+     try:
+          # test_logger_and_exception()
+          # get_collection_as_dataframe(database_name="flight_fare_prediction", collection_name="flight_fare")
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
+          data_ingestion = DataIngestion(data_ingestion_config= data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
 
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          logging.debug(str(e))
+          raise FlightException(e, sys)
